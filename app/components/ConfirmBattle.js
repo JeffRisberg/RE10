2326df06be3088a545687390c788d210f9b64cc1
JeffRisberg <jrisberg@aol.com>
2016-05-31 08:13:36 -0700

    updated to match last video and course materials

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -6,13 +6,14 @@ var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-    ? <p>LOADING</p>
+    ? <Loading />
     : <MainContainer>
         <h1>Confirm Players</h1>
-        <div className='col-sm-6 col-sm-offset-3'>
+        <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
             <UserDetails info={props.playersInfo[0]} />
           </UserDetailsWrapper>
@@ -20,7 +21,7 @@ function ConfirmBattle (props) {
             <UserDetails info={props.playersInfo[1]} />
           </UserDetailsWrapper>
         </div>
-        <div className='col-sm-6 col-sm-offset-3'>
+        <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
             <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
